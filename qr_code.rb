398dabbedcdb600f2c47c845c33fe28a9c68a3ea
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-24 18:26:37 -0400

    actually encoding the url

--- qr_code.rb
+++ qr_code.rb
@@ -3,7 +3,11 @@ require 'rqrcode'
 class QRCode
   def initialize(artist_name)
     @name = artist_name
-    @qrcode = RQRCode::QRCode.new(artist_name)
+    @qrcode = RQRCode::QRCode.new(url_for_artist)
+  end
+
+  def url_for_artist
+    "http://exfrasis.io/artist/#{@name}"
   end
 
   def write_to_file
